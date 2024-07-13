542b38885b0d84e3d9ad48188959692fc64f52d3
taruna-wd <tarunarajoriya18@gmail.com>
2024-07-13 12:13:18 +0530

    todo with js

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const button = document.getElementById("add");
+    let li = document.querySelector("li");
+
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      let input = document.querySelector("input");
+      console.log(input.value);
+      let ul = document.querySelector("ul");
+      console.log(ul.innerHTML);
+
+      let li = document.createElement("li");
+      if (input.value === "") {
+        alert("please enter some task");
+      } else {
+        li.innerHTML = `
+      ${input.value}
+      <button class="del btn btn-outline-danger">
+        <i class="fa-solid fa-xmark" id="check"></i>
+      </button>
+    `;
+        ul.appendChild(li);
+        input.value = "";
+      }
+
+      li.addEventListener("click", () => {
+        li.style.textDecoration =
+          li.style.textDecoration === "line-through" ? "none": "line-through";
+      });
+
+      li.querySelector(".del").addEventListener("click", (event) => {
+        event.stopPropagation(); // Prevent the click event from bubbling up to the li
+        ul.removeChild(li);
+      });
+
+      
+    });
+  });
+
+
+ 
+
+  
